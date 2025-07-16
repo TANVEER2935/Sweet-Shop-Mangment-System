6a1962b173a2075ebee743963ea7f441cc457edf
Your Name <tanrajput2935@gmail.com>
2025-07-16 15:57:29 +0530

    Implement delete sweet feature

--- src/sweetShop.js
+++ src/sweetShop.js
@@ -1,17 +1,24 @@
 class SweetShop {
-  constructor() {
-    this.sweets = [];
-  }
+    constructor() {
+        this.sweets = [];
+    }
 
-  addSweet(sweet) {
-    const exists = this.sweets.some(s => s.id === sweet.id);
-    if (exists) throw new Error("Sweet with this ID already exists.");
-    this.sweets.push(sweet);
-  }
+    addSweet(sweet) {
+        const exists = this.sweets.some(s => s.id === sweet.id);
+        if (exists) throw new Error("Sweet with this ID already exists.");
+        this.sweets.push(sweet);
+    }
 
-  getAllSweets() {
-    return this.sweets;
-  }
+    getAllSweets() {
+        return this.sweets;
+    }
+    deleteSweet(id) {
+        const index = this.sweets.findIndex(s => s.id === id);
+        if (index === -1) throw new Error("Sweet not found.");
+        this.sweets.splice(index, 1);
+    }
+
 }
+
 
 module.exports = SweetShop;
